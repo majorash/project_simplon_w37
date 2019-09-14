7c1abf0992edc27cbff4b2ee6e7530ec757ae52f
MatthieuOD <43931112+MatthieuOD@users.noreply.github.com>
2019-09-15 00:06:18 +0200

    Update customvision.py

--- Flask/project/customvision.py
+++ Flask/project/customvision.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 10:14:20 2019
+@author: melod
+"""
+
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 
 
 ENDPOINT = "https://southcentralus.api.cognitive.microsoft.com"
 
-training_key = "22ef4a89c10b4e0d8d6cdaabce6b7221"
-prediction_key = "467bd9a7c49a41f49a9cbf6846379e32"
-prediction_resource_id = "/subscriptions/f08d91e4-8ee9-45bb-b518-f36e0aecb653/resourceGroups/Castelnau/providers/Microsoft.CognitiveServices/accounts/Classifier_GroupProject_Prediction"
+training_key = ""customvision.py #training_key
+prediction_key = "" #prediction_key
+prediction_resource_id = "" #prediction_resource_id
 
 
 publish_iteration_name = "Iteration1"
@@ -15,7 +21,7 @@ publish_iteration_name = "Iteration1"
 trainer = CustomVisionTrainingClient(training_key, endpoint=ENDPOINT)
 predictor = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
 
-project = trainer.get_project('365bd7c3-f643-47c0-a0d3-91f7fb50a65a')
+project =  #projetcid
 
 def classify_photo(url):
 
